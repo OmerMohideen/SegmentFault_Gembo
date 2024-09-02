5fa6bb500abead261e988ba0ae52637aad0fc7d2
OmerMohideen <omermohideen2004@gmail.com>
2024-09-02 21:23:08 +0530

    feat(style): primary and secondary subtle colors

--- user_application/lib/styles/colors.dart
+++ user_application/lib/styles/colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xff5DB075);
+const Color primarySubtleColor = Color(0xffEAF5ED);
 const Color secondaryColor = Color(0xffF46B3A);
+const Color secondarySubtleColor = Color(0xffFDE2D8);
 const Color tertiaryColor = Color(0xffF4ECD9);
