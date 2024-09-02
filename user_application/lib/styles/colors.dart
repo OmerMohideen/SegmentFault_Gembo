13fe37b1c2cbbbfdf5e33b343e02aad850f31c75
OmerMohideen <omermohideen2004@gmail.com>
2024-09-02 14:19:00 +0530

    style(user): style guide variables

---
+++ user_application/lib/styles/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xff5DB075);
+const Color secondaryColor = Color(0xffF46B3A);
+const Color tertiaryColor = Color(0xffF4ECD9);
---
+++ user_application/lib/styles/fonts.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle displayStyle = GoogleFonts.inter(
+  fontSize: 36,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle heading1Style = GoogleFonts.inter(
+  fontSize: 28,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle heading2Style = GoogleFonts.inter(
+  fontSize: 22,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle paragraph1Style = GoogleFonts.inter(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle paragraph2Style = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle buttonStyle = GoogleFonts.inter(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
