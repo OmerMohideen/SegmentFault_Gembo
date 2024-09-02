f8eb7d65d502e7abb70aca2473531edfc6e994d1
OmerMohideen <omermohideen2004@gmail.com>
2024-09-02 17:13:46 +0530

    feat(user): back button widget

---
+++ user_application/lib/widgets/back_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DripBackButton extends StatelessWidget {
+  final void Function()? onTap;
+
+  const DripBackButton({super.key, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: this.onTap,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          SvgPicture.asset(
+            'assets/icons/drip_button_base.svg',
+            width: 40.0,
+            height: 40.0,
+          ),
+          Positioned(
+            top: 12,
+            child: SvgPicture.asset(
+              'assets/icons/arrow_back.svg',
+              width: 8.5,
+              height: 12.5,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
