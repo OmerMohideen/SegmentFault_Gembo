fd241f14d2484dda92afa210d82906cc74c85469
OmerMohideen <omermohideen2004@gmail.com>
2024-09-04 02:26:21 +0530

    fix(user): fixed alignment and made widget reusable

--- user_application/lib/widgets/country_picker.dart
+++ user_application/lib/widgets/country_picker.dart
@@ -1,7 +1,17 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 import 'package:country_picker/country_picker.dart';
+import 'package:user_application/styles/fonts.dart';
 
 class CountryPicker extends StatelessWidget {
+  final Function(Country) onSelect;
+  final String title;
+
+  const CountryPicker({
+    Key? key,
+    required this.onSelect,
+    required this.title
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -9,18 +19,20 @@ class CountryPicker extends StatelessWidget {
         showCountryPicker(
           showPhoneCode: true,
           context: context,
-          onSelect: (value) {
-            print(value);
-          },
+          onSelect: onSelect,
         );
       },
       child: Container(
-        height: 65,
-        width: double.infinity,
+        alignment: Alignment.topCenter,
+        height: 38,
+        margin: EdgeInsets.only(top: 18),
         decoration: BoxDecoration(
-            border:
-                Border(bottom: BorderSide(color: Color(0xffBDBDBD), width: 1))),
-        child: Text("Select country code"),
+          border: Border(bottom: BorderSide(color: Colors.black, width: 1)),
+        ),
+        child: Text(
+          title,
+          style: paragraph1Style.copyWith(color: Colors.black),
+        ),
       ),
     );
   }
